981a0efe6c5df40055c8e29c9a01efa7f55c65c0
developertariq <dev.tariqul.islam@gmail.com>
2022-12-20 12:41:13 +0600

    Configure linters for html+css+js project

--- src/index.js
+++ src/index.js
@@ -4,10 +4,8 @@ import './style.css';
 function component() {
   const element = document.createElement('div');
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-  
+  element.innerHTML = _.join(['Recent', 'scores'], ' ');
+
   return element;
 }
 
