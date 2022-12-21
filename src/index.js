9c05616197a64bf1c7771a466931319621c895d5
developertariq <dev.tariqul.islam@gmail.com>
2022-12-22 01:27:21 +0600

    Clear form after add data

--- src/index.js
+++ src/index.js
@@ -7,12 +7,18 @@ const myList = document.getElementById('leaderboard');
 const form = document.getElementById('entry-form');
 const btnRefresh = document.getElementById('btnRefresh');
 
+const clearForm = () => {
+  form.elements.name.value = '';
+  form.elements.score.value = '';
+};
+
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   const users = {};
   users.user = form.elements.name.value;
   users.score = form.elements.score.value;
   add(gameId, users, myList);
+  clearForm();
 });
 
 btnRefresh.addEventListener('click', () => {
